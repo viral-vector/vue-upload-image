9f701ef1ddc0de0051aec0a6a2be60b542eb7a52
Viral-Vector <vectorviral@gmail.com>
2017-01-25 04:04:36 -0500

    release: 1.0.0
    -- initial release of uploader

--- README.md
+++ README.md
@@ -1,36 +1,25 @@
-# vue-image-uploader [pre-alpha]
+# vue-image-uploader
 Configurable image preview and ajax up-uploader
 
-## Mission Goals
  + drag and drop with input backup
- + non blokcing 
  + image previews
- - events
- - 0 dependencies..except vuejs
-    - 1 dependency [vue-resource]
- * minimal
- * configurable
-
-## TODO
- * trim component html
- * move response handling to events
- * dispatch events
- * remove vue-resource dependency
- * batched & parallel uploading
- * auto submission
- * more configurations 
+ + events
+ + 1 dependency [vue-resource]
+ + minimal
+ + configurable
+ + batched or async
 
 ## Installation
 
 ### NPM
 ```
-npm install vue-image-upload --save
+npm install vue-image-uploader --save
 ```
 
 ## Utilization 
 ### ES6
 ```js
-    import ImageUpload from 'vue-image-upload';
+    import ImageUpload from 'vue-image-uploader';
 
     new Vue({
         template: '<image-upload url=""></image-upload>',
@@ -38,7 +27,45 @@ npm install vue-image-upload --save
             ImageUpload
         }
     })
-
+```
+```html
+    <image-upload url="" name="" max_files=""></image-upload>
 ```
 
 ## Configuration
+
+    url: {
+        type: String,
+        required: true,
+        default: null
+    },
+    name: {
+        type: String,
+        required: false,
+        default: 'images[]'
+    },
+    max_batch: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    max_files: {
+        type: Number,
+        required: false,
+        default: 10
+    },
+    max_filesize: {
+        type: Number,
+        required: false,
+        default: 5000
+    },
+    button_html: {
+        type: String,
+        required: false,
+        default: 'Upload Images'
+    },
+    button_class: {
+        type: String,
+        required: false,
+        default: 'btn btn-primary'
+    }
--- dist/uploader.vue
+++ dist/uploader.vue
@@ -1,73 +1,86 @@
 <template>
-    <div class="container">
-        <div class="vue_component vue_component__imageupload" v-bind:class="{ 'dragover': onDragover }">
-            <form class="form" id="image_upload_form" action="" enctype="multipart/form-data">
-                <div class="image_upload_form__thumbnails">
-                    <div v-for="(value, key) in files" v-bind:class="{ 'uploaded': value.uploaded }">
-                        <span v-on:click="fileRemove(key)">&#x2716;</span>
-                        <img v-bind:src="image[key]" v-bind:class="{ 'show': image[key] }">
-                    </div>
+    <div class="vue_component vue_component__imageupload" v-bind:class="{ 'dragover': onDragover }">
+        <form v-bind:id="'image_upload_form--' + name" enctype="multipart/form-data">
+            <div class="image_upload_form__thumbnails">
+                <div v-for="(value, key) in files" class="image_upload_form__thumbnail" v-on:click="fileView($event, key)" 
+                        v-bind:class="{ 'uploaded': value.uploaded, 'bad-size': value.bad_size }" >
+                    <span v-on:click="fileDelete($event, key)">
+                    &#x2716;
+                    </span>
+                    <img v-bind:src="image[key]" v-bind:class="{ 'show': image[key]}">
                 </div>
-                <div class="form__input hidden">
-                    <input type="file" name="files[]" id="image_upload_form__input" multiple />
-                </div>
-                <div class="form__button padding-top-5">  
-                    <button type="submit" class="btn btn-primary" v-on:click="submit" v-bind:disabled="onUploading">
-                        <i class="fa fa-check fa-prefix"></i>{{ message_confirm }} {{ queue }} Images
-                    </button>
-                </div>
-            </form>
-        </div>
+            </div>
+            <input type="file" v-bind:id="'image_upload_form__input--' + name" hidden multiple />
+            <div>  
+                <button type="submit" 
+                    v-bind:class="button_class" 
+                    v-on:click="submit" 
+                    v-bind:disabled="onUploading" 
+                    v-html="button_html"></button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'image_upload',
+        name: 'ImageUpload',
         props: {
             url: {
                 type: String,
                 required: true,
                 default: null
             },
+            name: {
+                type: String,
+                required: false,
+                default: 'images[]'
+            },
+            max_batch: {
+                type: Number,
+                required: false,
+                default: 0
+            },
             max_files: {
                 type: Number,
                 required: false,
-                default: -1
+                default: 10
             },
             max_filesize: {
                 type: Number,
                 required: false,
-                default: -1
+                default: 5000
             },
-            confirm_text: {
+            button_html: {
                 type: String,
                 required: false,
-                default: 'Upload!'
-            },       
+                default: 'Upload Images'
+            },
+            button_class: {
+                type: String,
+                required: false,
+                default: 'btn btn-primary'
+            },
         },
         data: function(){
             return {
                 form: null,
                 input: null,
-                click: null,
                 index: 0,
-                queue: 0,
                 total: 0,
                 files: {}, 
                 image: {},
+                batch: {},
                 onDragover: false,
                 onUploading: false  
             }
         },
         mounted: function(){
-            this.form = document.getElementById('image_upload_form');
-            this.input = document.getElementById('image_upload_form__input');
-            this.click = document.getElementById('image_upload_form__click');
+            this.form = document.getElementById('image_upload_form--' + this.name);
+            this.input = document.getElementById('image_upload_form__input--' + this.name);
 
             ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(event => this.form.addEventListener(event, (e) => {
-                e.preventDefault();
-                e.stopPropagation();
+                e.preventDefault(); e.stopPropagation();
             }));
 
             ['dragover', 'dragenter']
@@ -82,46 +95,104 @@
             ['change']
                 .forEach(event => this.input.addEventListener(event, this.fileDrop));
 
-            this.click.addEventListener('click', (e) => {
+            this.form.addEventListener('click', (e) => {
                 this.input.click();
             });
         },
         methods: {
-            submit: function(e){
-                e.preventDefault();
-                
-                if(!this.onUploading){
-                    this.upload();
+            _can_xhr(){
+                if(this.total >= this.max_files){
+                    return false;
                 }
+                return true;
             },
-            upload: function(){
-                for (let key in this.files) {
-                    if(this.files[key].uploaded || this.queue <= 0) continue;
+            _can_upload_file(key){
+                let file = this.files[key];
 
-                    this.onUploading = true;
+                if(file.attempted || file.bad_size){
+                    return false;
+                }
+                return true;
+            }, 
+            _xhr: function(formData, keys, callback){
+                this.onUploading = true;
+                this.$emit('image-upload-attempt', formData);
 
-                    let formData = new FormData();
-                    formData.append(this.input.getAttribute('name'), this.files[key]); 
-                    
-                    this.$http.post(this.url, formData).then((response) => {
+                keys.forEach((key) => {
+                    Vue.set(this.files[key], 'attempted', true);
+                });
+
+                this.$http.post(this.url, formData).then((response) => {
+                    keys.forEach((key) => {
                         Vue.set(this.files[key], 'uploaded', true);
 
-                        this.queue--;
                         this.total++;
-                    }, (response) => {
-                        
-                    }).then((response) => {
-                        this.upload();
                     });
-                    
-                    return false;   
-                }
 
-                this.onUploading = false;     
+                    this.$emit('image-upload-success', [formData]);
+                }, (response) => {
+                    this.$emit('image-upload-failure', [response]);
+                }).then((response) => {
+                    this.onUploading = false;  
+                    
+                    callback(); 
+                });
             },
-            canDragDrop: function(){
-                let div = document.createElement('div');
-                return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+            upload: function(){
+                if(!this._can_xhr()) return false;
+
+                for (let key in this.files) {
+                    if(!this._can_upload_file(key)) continue;
+
+                    let formData = new FormData();
+                    formData.append(this.name, this.files[key]); 
+                    
+                    this._xhr(formData, [key], this.upload);
+                    
+                    return true;   
+                }  
+            },
+            upload_batch: function(){
+                if(!this._can_xhr()) return false;
+
+                for (let key in this.batch) {                    
+                    this._xhr(this.batch[key].form, this.batch[key].keys, this.upload_batch); 
+
+                    delete this.batch[key];
+
+                    return true;
+                } 
+            },
+            create_batch: function(){
+                let index = 0;
+                let count = 0;
+
+                this.batch = {};
+
+                for (let key in this.files) {
+                    if(!this._can_upload_file(key)) continue;
+
+                    if(this.batch[index] == null || count == this.max_batch){
+                        index++;
+                        count = 0;
+                        this.batch[index] = {form:new FormData(), keys:[]};
+                    }
+
+                    count++;
+                    this.batch[index]['keys'].push(key);
+                    this.batch[index]['form'].append(this.name, this.files[key]);
+                }
+            },
+            submit: function(e){
+                e.preventDefault(); e.stopPropagation();
+                
+                if(!this.onUploading){
+                    if(this.max_batch > 1){
+                               this.create_batch();
+                        return this.upload_batch();                        
+                    }
+                    this.upload();
+                }
             },
             dragEnter: function(e){
                 e.preventDefault();
@@ -134,34 +205,107 @@
             fileDrop: function(e){
                 e.preventDefault();
 
-                let newFiles = e.target.files || e.dataTransfer.files || e.originalEvent.dataTransfer.files;
+                let newFiles = e.target.files || e.dataTransfer.files;
 
                 for(let i = 0; i < newFiles.length; i++){
-                    this.files[this.index] = newFiles[i];
+                    Vue.set(this.files, this.index, newFiles[i]);
+                    this.fileInit(this.index);
+                    this.fileRead(this.index);
 
-                    this.fileLoad(this.index);
-
-                    this.index++;
-                    this.queue++; 
+                    this.index++; 
                 }
 
-                e.target.value = ''; // clear file input for same file submission
+                e.target.value = '';
             },
-            fileLoad: function(index){
+            fileInit: function(key){
+                let file = this.files[key];
+
+                if((file.size * 0.001) > this.max_filesize){
+                    Vue.set(this.files[key], 'bad_size', true);
+                }
+            },
+            fileRead: function(key){
                 let reader = new FileReader();
 
                 reader.addEventListener("load", (e) => {
-                    Vue.set(this.image, index, reader.result);
+                    Vue.set(this.image, key, reader.result);
                 });
 
-                reader.readAsDataURL(this.files[index]);
+                reader.readAsDataURL(this.files[key]);
             },
-            fileRemove: function(key){    
-                this.queue -= this.files[key].uploaded? 0:1;
-                
+            fileDelete: function(e, key){
                 Vue.delete(this.files, key);
                 Vue.delete(this.image, key);
+
+                this.$emit('image-upload-image-delete', [ this.files[key]]);
             },
+            fileView: function(e, key){
+                e.preventDefault(); e.stopPropagation();
+
+                this.$emit('image-upload-image-view', [ this.files[key]]);
+            }
         }
     }
 </script>
+
+<style lang="sass" scoped>
+    .vue_component__imageupload{
+        padding: 5px;
+        cursor: pointer;
+        min-height: 80px;
+        border-radius: 5px;
+        &.dragover{
+        }
+        form > div{
+            text-align: center;
+        }
+        .image_upload_form__thumbnails{ 
+            margin-bottom: 1em;
+            .image_upload_form__thumbnail{
+                border-radius: 2.5px;
+                position:relative;
+                width:20%;
+                padding:20% 0 0;
+                overflow: hidden;
+                margin:10px;
+                display:inline-block;
+                img{
+                    position: absolute;
+                    top:50%;
+                    left: 50%;
+                    min-width: 100%;
+                    min-height: 100%;
+                    max-height: 150%;
+                    opacity: 0;
+                    transform: translateX(-50%) translateY(-50%);
+                    transition: 1s opacity;
+                    &.show{
+                        opacity: 1;
+                    }
+                    &:hover{
+                        filter: blur(2px);
+                    }
+                }
+                span{
+                    position: absolute;
+                    top: -5px;
+                    left: 0px;  
+                    z-index: 100;
+                    padding: 0px 1px;
+                    border-radius: 2px;
+                    background-color: grey;
+                }
+                &.bad-size{
+                    img{
+                        filter: grayscale(100%);  
+                    }
+                }
+                &.uploaded{
+                    img{
+                        opacity: 0.1;
+                    }
+                }
+            }
+        }
+    }
+</style>
